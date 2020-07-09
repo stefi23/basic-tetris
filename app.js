f527ed030cda7b4ec079d4d98f6f0c61bf425bb4
stefi23 <stefania.rosca2309@gmail.com>
2020-07-09 22:37:44 +0200

    added the tetrominos

--- app.js
+++ app.js
@@ -7,4 +7,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
   console.log("grid: ", grid);
   console.log("squares: ", squares);
+
+  // The Tetrominos
+
+  const lTetromino = [
+    [1, witdh + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
+
+  const zTetromino = [
+    [1, 2, width, width + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [1, 2, width, width + 1],
+    [0, width, width + 1, width * 2 + 1],
+  ];
+
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [0, 1, 2, 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [0, 1, 2, 3],
+  ];
 });
